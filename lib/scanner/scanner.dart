4fa45b4cc56e7cf0eb59fc0b9761c38fc00cd72d
hadi-14 <hadimillwala@gmail.com>
2024-08-30 22:09:20 +0500

    updated scanner

--- lib/scanner/scanner.dart
+++ lib/scanner/scanner.dart
@@ -21,7 +21,76 @@ class _BarcodeScannerWithZoomState extends State<BarcodeScannerWithZoom>
   );
 
   bool _isProcessingBarcode = false;
+  bool _allowScan = false; // Controls when scanning is allowed
   double _zoomFactor = 0.0;
+  List<Map<String, String>> _scannedProducts = []; // Stores UPC code and product names
+
+  @override
+  void initState() {
+    controller.start(); // Ensure the camera preview is always on
+    super.initState();
+  }
+
+  Future<void> _fetchProductName(String barcode) async {
+    final userState = Provider.of<UserState>(context, listen: false);
+    final CollectionReference productsCollection = FirebaseFirestore.instance
+        .collection('product')
+        .doc(userState.franchiseID)
+        .collection('list');
+
+    try {
+      final querySnapshot = await productsCollection
+          .where('upcCode', isEqualTo: barcode)
+          .limit(1)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        final productName = querySnapshot.docs.first['productName'] as String;
+        setState(() {
+          _scannedProducts.insert(0, {'upcCode': barcode, 'productName': productName});
+          if (_scannedProducts.length > 5) {
+            _scannedProducts = _scannedProducts.sublist(0, 5);
+          }
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Product with UPC $barcode not found'),
+            duration: const Duration(seconds: 2),
+          ),
+        );
+      }
+    } catch (e) {
+      print('Error fetching product name: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error fetching product name: $e'),
+          duration: const Duration(seconds: 2),
+        ),
+      );
+    }
+  }
+
+  void _onDetect(BarcodeCapture capture) async {
+    if (_isProcessingBarcode || !_allowScan) return; // Prevent re-entry if already processing or not allowed to scan
+    final barcode = capture.barcodes.first.rawValue;
+    if (barcode != null) {
+      setState(() {
+        _isProcessingBarcode = true;
+        _allowScan = false; // Disallow further scanning until button is pressed again
+      });
+      await _fetchProductName(barcode); // Fetch product name using the barcode
+      setState(() {
+        _isProcessingBarcode = false;
+      });
+    }
+  }
+
+  void _enableScan() {
+    setState(() {
+      _allowScan = true; // Enable scanning for one barcode
+    });
+  }
 
   Widget _buildZoomScaleSlider() {
     return ValueListenableBuilder(
@@ -31,23 +100,19 @@ class _BarcodeScannerWithZoomState extends State<BarcodeScannerWithZoom>
           return const SizedBox.shrink();
         }
 
-        final TextStyle labelStyle = Theme.of(context)
-            .textTheme
-            .headlineMedium!
-            .copyWith(color: Colors.white);
-
         return Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
           child: Row(
             children: [
               Text(
                 '0%',
-                overflow: TextOverflow.fade,
-                style: labelStyle,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Colors.white),
               ),
               Expanded(
                 child: Slider(
                   value: _zoomFactor,
+                  activeColor: const Color(0xFFD09A6C),
+                  inactiveColor: Colors.grey[300],
                   onChanged: (value) {
                     setState(() {
                       _zoomFactor = value;
@@ -58,8 +123,7 @@ class _BarcodeScannerWithZoomState extends State<BarcodeScannerWithZoom>
               ),
               Text(
                 '100%',
-                overflow: TextOverflow.fade,
-                style: labelStyle,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Colors.white),
               ),
             ],
           ),
@@ -68,70 +132,43 @@ class _BarcodeScannerWithZoomState extends State<BarcodeScannerWithZoom>
     );
   }
 
-  void _onDetect(BarcodeCapture capture) async {
-    final userState = Provider.of<UserState>(context, listen: false);
-
-    if (_isProcessingBarcode) return; // Prevent re-entry if already processing
-    final barcode = capture.barcodes.first.rawValue;
-    if (barcode != null) {
-      setState(() {
-        _isProcessingBarcode = true;
-      });
-
-      // Assume 'products' collection exists with a document for the scanned UPC
-      final CollectionReference productsCollection =
-          FirebaseFirestore.instance.collection('product').doc(userState.franchiseID).collection('list');
-
-      try {
-        // Fetch product with the scanned UPC code
-        final productSnapshot = await productsCollection
-            .where('upcCode', isEqualTo: barcode)
-            .limit(1)
-            .get();
-
-        if (productSnapshot.docs.isNotEmpty) {
-          // Update product with new data (example: increment stock count)
-          final productDoc = productSnapshot.docs.first;
-          await productsCollection.doc(productDoc.id).update({
-            'quantity': FieldValue.increment(1), // Example: Increment stock count
-          });
-
-          // Show a snackbar indicating that the product was updated
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text('Product with UPC $barcode updated successfully'),
-              duration: const Duration(seconds: 2),
+  Widget _buildScannedProductsList() {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      itemCount: _scannedProducts.length,
+      itemBuilder: (context, index) {
+        final product = _scannedProducts[index];
+        return Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          elevation: 2,
+          child: ListTile(
+            title: Text(
+              product['productName'] ?? 'Unknown Product',
+              style: const TextStyle(
+                fontSize: 16,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF353934),
+              ),
             ),
-          );
-        } else {
-          // If product does not exist, show an error message
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text('Product with UPC $barcode not found'),
-              duration: const Duration(seconds: 2),
+            subtitle: Text(
+              'UPC: ${product['upcCode']}',
+              style: const TextStyle(
+                fontSize: 14,
+                fontFamily: 'Poppins',
+                color: Color(0xFF707070),
+              ),
             ),
-          );
-        }
-      } catch (e) {
-        // Handle errors, such as network issues
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Error updating product: $e'),
-            duration: const Duration(seconds: 2),
           ),
         );
-      } finally {
-        // Navigate back after a short delay
-        // Future.delayed(const Duration(seconds: 1), () {
-          Navigator.pop(context, barcode);
-        // });
-      }
-    }
+      },
+    );
   }
 
   Future<bool> _onWillPop() async {
     if (_isProcessingBarcode) {
-      // Show a message to the user
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Processing barcode, please wait...')),
       );
@@ -145,101 +182,139 @@ class _BarcodeScannerWithZoomState extends State<BarcodeScannerWithZoom>
     super.build(context);
     final size = MediaQuery.of(context).size;
 
-    return WillPopScope(
-      onWillPop: _onWillPop,
-      child: Scaffold(
-        backgroundColor: const Color(0xFFFAFAFA),
-        body: Stack(
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text('Scan'),
+        backgroundColor: const Color(0xFFD09A6C),
+      ),
+      body: WillPopScope(
+        onWillPop: _onWillPop,
+        child: Column(
           children: [
-            Positioned(
-              left: 0,
-              top: 0,
+            // Camera preview with full width
+            Container(
+              width: size.width,
+              height: size.height * 0.5, // Adjusted height for the camera preview
+              decoration: const BoxDecoration(
+                color: Color(0xFFFAFAFA),
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      width: size.width,
+                      height: size.height * 0.4,
+                      margin: const EdgeInsets.all(20),
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(width: 1, color: Color(0xFFE0E0E0)),
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                      ),
+                      child: MobileScanner(
+                        controller: controller,
+                        fit: BoxFit.contain,
+                        onDetect: _onDetect,
+                        errorBuilder: (context, error, child) {
+                          return ScannerErrorWidget(error: error);
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Scanned history list with full width in a rounded container
+            Expanded(
               child: Container(
                 width: size.width,
-                height: size.height,
-                decoration: const BoxDecoration(color: Color(0xFFFAFAFA)),
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    topRight: Radius.circular(24),
+                  ),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 6,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
                 child: Column(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.arrow_back, color: Colors.black),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                          const Text(
-                            'Scan',
-                            style: TextStyle(
-                              color: Color(0xFF353934),
-                              fontSize: 18,
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          const SizedBox(width: 20), // Placeholder for alignment
-                        ],
+                    const Text(
+                      'Scanned History',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                     Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.all(20),
-                        decoration: ShapeDecoration(
-                          shape: RoundedRectangleBorder(
-                            side: const BorderSide(width: 1, color: Color(0xFFE0E0E0)),
-                            borderRadius: BorderRadius.circular(24),
-                          ),
-                        ),
-                        child: MobileScanner(
-                          controller: controller,
-                          fit: BoxFit.contain,
-                          onDetect: _onDetect,
-                          errorBuilder: (context, error, child) {
-                            return ScannerErrorWidget(error: error);
-                          },
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: _buildZoomScaleSlider(),
+                      child: _buildScannedProductsList(), // Display scanned products list
                     ),
                   ],
                 ),
               ),
             ),
-            Positioned(
-              bottom: 20,
-              left: size.width * 0.2,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // Camera controls with zoom slider
+            Container(
+              width: size.width,
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 6,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Column(
                 children: [
-                  IconButton(
-                    icon: const Icon(Icons.qr_code_scanner, color: Color(0xFFD09A6C)),
-                    onPressed: () {},
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: _buildZoomScaleSlider(),
                   ),
-                  const SizedBox(width: 54),
-                  Container(
-                    width: 60,
-                    height: 60,
-                    decoration: const ShapeDecoration(
-                      color: Color(0xFFD09A6C),
-                      shape: CircleBorder(),
-                    ),
-                    child: IconButton(
-                      icon: const Icon(Icons.camera_alt, color: Colors.white),
-                      onPressed: () {},
-                    ),
-                  ),
-                  const SizedBox(width: 54),
-                  IconButton(
-                    icon: const Icon(Icons.flash_on, color: Color(0xFFD09A6C)),
-                    onPressed: () {
-                      controller.toggleTorch();
-                    },
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.flip_camera_ios, color: Color(0xFFD09A6C)),
+                        onPressed: () {
+                          controller.switchCamera();
+                        },
+                      ),
+                      Container(
+                        width: 60,
+                        height: 60,
+                        decoration: const ShapeDecoration(
+                          color: Color(0xFFD09A6C),
+                          shape: CircleBorder(),
+                        ),
+                        child: IconButton(
+                          icon: const Icon(Icons.camera_alt, color: Colors.white),
+                          onPressed: _enableScan, // Allow scanning when camera button is pressed
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.flash_on, color: Color(0xFFD09A6C)),
+                        onPressed: () {
+                          controller.toggleTorch();
+                        },
+                      ),
+                    ],
                   ),
                 ],
               ),
